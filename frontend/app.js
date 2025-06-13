f0e2c40efd7464fbd172f28b4058f9666e7f739a
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2025-06-14 01:35:30 +0200

    Added a js file to fetch data from db and load them into html file

---
+++ frontend/app.js
@@ -0,0 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const db = new sqlite3.Database(path.join(__dirname, "../models/transactions.db"));
+
+db.all("SELECT date, amount, sender, recipient_name, transaction_type FROM transactions ORDER BY date DESC;", (err, rows) => {
+    if (err) return console.error("Database Error:", err);
+    
+    // Save results to a JSON file
+    const fs = require("fs");
+    fs.writeFileSync("transactions.json", JSON.stringify(rows, null, 2));
+
+    console.log("✅ Transactions saved to transactions.json");
+});
