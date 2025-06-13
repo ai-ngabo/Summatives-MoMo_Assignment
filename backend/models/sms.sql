7174e3dccc85493545419a25dfe0e4c962f73ae3
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2025-06-14 00:15:06 +0200

    updated sql file

--- backend/models/sms.sql
+++ backend/models/sms.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    body TEXT NOT NULL,
     date TEXT NOT NULL,
     amount INTEGER NOT NULL,
     sender TEXT,
     recipient_name TEXT,
     recipient_phone TEXT,
     fee INTEGER DEFAULT 0,
-    new_balance INTEGER NOT NULL,
+    new_balance INTEGER,
     transaction_type TEXT,
-    UNIQUE(body, date)
+    UNIQUE(date, amount, sender)
 );
